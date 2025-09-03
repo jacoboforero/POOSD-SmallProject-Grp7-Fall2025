1aba32c0b407c73b74b1ae6653b80ce8fb867df1
br549625 <br549625@ucf.edu>
2025-09-03 17:14:09 -0400

    Added APIs

---
+++ app/api/UpdateContact.php
@@ -0,0 +1,43 @@
+<?php
+	$inData = getRequestInfo();
+	
+    $id = $inData["id"];
+	$firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		tmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE ID = ? AND UserID = ?");
+		$stmt->bind_param("ssssss", $firstName, $lastName, $phone, $email, $id, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
